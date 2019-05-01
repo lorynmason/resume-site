da5d42cff29803b8676fdc553e2e2071933e0359
Loryn Mason <masonloryn@gmail.com>
2019-05-01 16:00:54 -0700

    Style Name

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,7 +5,8 @@ function Home() {
   return (
     <main>
       <div className="name">
-        <h1>LORYN MASON</h1>
+        <h1>LORYN</h1>
+        <h1>MASON</h1>
         <h3>FULL STACK DEVELOPER</h3>
       </div>
       <Menu />
