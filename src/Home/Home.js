5adbcf8a7c7a2f1c673a8cca4bec799159868efe
Loryn Mason <masonloryn@gmail.com>
2019-05-01 15:32:37 -0700

    Add menu component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Menu from '../Menu/Menu';
 
 function Home() {
   return (
     <main>
       <h1>LORYN MASON</h1>
       <h3>FULL STACK DEVELOPER</h3>
+      <Menu />
     </main>
   );
 }
---
+++ src/Menu/Menu.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <nav>
+      <div className="nav-block">
+        <h4>H</h4>
+        <h6>History</h6>
+      </div>
+      <div className="nav-block">
+        <h4>S</h4>
+        <h6>Skills</h6>
+      </div>
+      <div className="nav-block">
+        <h4>P</h4>
+        <h6>Projects</h6>
+      </div>
+      <div className="nav-block">
+        <h4>G</h4>
+        <h6>Goals</h6>
+      </div>
+      <div className="nav-block">
+        <h4>A</h4>
+        <h6>About Me</h6>
+      </div>
+    </nav>
+  );
+}
+
+export default Menu;
