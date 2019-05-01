e13f1b9af0a6e38617908fa51a5cb112d0ad68e0
Loryn Mason <masonloryn@gmail.com>
2019-05-01 15:11:48 -0700

    Setup Home component

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <main>
+      <h1>LORYN MASON</h1>
+      <h3>FULL STACK DEVELOPER</h3>
+    </main>
+  );
+}
+
+export default Home;
