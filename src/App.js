9f5b9f037bb1e54a5af01a67c1cdba58f0c14321
Loryn Mason <masonloryn@gmail.com>
2019-05-02 13:22:43 -0700

    Add timeline

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Home from './Home/Home';
 import "./styles/main.scss";
 import { Route, Switch } from 'react-router-dom';
+import History from './History/History';
 
 function App() {
   return (
     <div className="App">
         <Switch> 
           <Route exact path="/" component={Home}/>
-          {/* <Route path="/lodging" component={Lodge}/>
-          <Route path="/skiff-rentals" component={Skiff}/>
-          <Route path="/other-services" component={Other}/>
-          <Route path="/about" component={About}/> */}
+          <Route exact path="/history" component={History}/>
         </Switch> 
     </div>
   );
---
+++ src/History/History.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import Menu from '../Menu/Menu';
+
+function History() {
+  return (
+    <main>
+     <section className="timeline">
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+      <div className='marker'>
+        <div className='content'>
+          <h1>2007</h1>
+          <p>first job</p>
+        </div>
+      </div>
+     </section>
+    </main>
+  );
+}
+
+export default History;
