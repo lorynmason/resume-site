922f96d12493bf19058e7e8098dd9e1e684ba0ef
Loryn Mason <masonloryn@gmail.com>
2019-05-02 15:01:29 -0700

    Add Header

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header>
+        <div className="header-name">
+        <h1>LORYN</h1>
+        <h1>MASON</h1>
+        <h3>FULL STACK DEVELOPER</h3>
+      </div>
+    </header>
+  )
+}
