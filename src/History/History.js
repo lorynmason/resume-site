c482fde61b49bccacdd8fb37e62d99ffdebefe2f
Loryn Mason <masonloryn@gmail.com>
2019-05-02 13:58:51 -0700

    Add timeline content, incomplete

--- src/History/History.js
+++ src/History/History.js
@@ -8,49 +8,51 @@ function History() {
       <div className='marker'>
         <div className='content'>
           <h1>2007</h1>
-          <p>first job</p>
+          <p>
+            I got my first job washing dishes part time at the Southeast Waffle Company, a small cafe in Juneau, Alaska. Within 6 months I was promonted and started working in the kitchen, and slowly gain more responsiblity. In 2010, I was promonted to store manger.   
+          </p>
         </div>
       </div>
       <div className='marker'>
         <div className='content'>
-          <h1>2007</h1>
-          <p>first job</p>
+          <h1>2010</h1>
+          <p>Graduated High School and start working on Bachelor of Science in Business Administration at the University of Alaska</p>
         </div>
       </div>
       <div className='marker'>
         <div className='content'>
-          <h1>2007</h1>
-          <p>first job</p>
+          <h1>2013</h1>
+          <p>Took a break from working on my Bachelor's degree to attend culinary School at the art institute of Porland, where I graduated with baking and pastry certificate in March 2014</p>
         </div>
       </div>
       <div className='marker'>
         <div className='content'>
-          <h1>2007</h1>
-          <p>first job</p>
+          <h1>2014</h1>
+          <p>Moved back to Alaska to work road construction at Glacier State Contractors for 6 months to save up move to finish my BSBA.</p>
         </div>
       </div>
       <div className='marker'>
         <div className='content'>
-          <h1>2007</h1>
-          <p>first job</p>
+          <h1>2014</h1>
+          <p>Moved back to Portland to attend Portand State University. One of my required classes</p>
         </div>
       </div>
       <div className='marker'>
         <div className='content'>
-          <h1>2007</h1>
-          <p>first job</p>
+          <h1>2016</h1>
+          <p>Graduated Portand State University with my BSBA. shortly after moved to Eugene, OR.</p>
         </div>
       </div>
       <div className='marker'>
         <div className='content'>
-          <h1>2007</h1>
-          <p>first job</p>
+          <h1>2016</h1>
+          <p>BAKER | COFFEE PLANT ROASTER | SEPTEMBER 2016-AUGUST 2017</p>
         </div>
       </div>
       <div className='marker'>
         <div className='content'>
-          <h1>2007</h1>
-          <p>first job</p>
+          <h1>2017</h1>
+          <p>BAKER | 100 MILE BAKERY | AUGUST 2017-MARCH 2018</p>
         </div>
       </div>
       <div className='marker'>
