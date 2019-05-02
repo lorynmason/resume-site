db25dc7b2822ec2f0c233ea71716e68870031cf2
Loryn Mason <masonloryn@gmail.com>
2019-05-02 16:05:43 -0700

    Add navigation to header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <header>
-        <div className="header-name">
-        <h1>LORYN</h1>
-        <h1>MASON</h1>
+      <div className="header-name">
+        <h1>LORYN MASON</h1>
         <h3>FULL STACK DEVELOPER</h3>
       </div>
+      <nav className='header-nav'>
+      <NavLink to='/history'>
+        <div className="nav-block block1">
+          <h4>H</h4>
+          <h6>History</h6>
+        </div>
+      </NavLink>
+      <div className="nav-block block2">
+        <h4>S</h4>
+        <h6>Skills</h6>
+      </div>
+      <div className="nav-block block3">
+        <h4>P</h4>
+        <h6>Projects</h6>
+      </div>
+      <div className="nav-block block4">
+        <h4>G</h4>
+        <h6>Goals</h6>
+      </div>
+      <div className="nav-block block5">
+        <h4>A</h4>
+        <h6>About</h6>
+      </div>
+      </nav>
     </header>
   )
 }
