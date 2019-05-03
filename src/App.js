b275d7f093b36f863c30ea573743025778afb626
Loryn Mason <masonloryn@gmail.com>
2019-05-03 11:17:01 -0700

    Add route for projects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles/main.scss";
 import { Route, Switch } from 'react-router-dom';
 import History from './History/History';
 import { Skills } from './Skills/Skills'
+import { Projects } from './Projects/Projects';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" component={Home}/>
           <Route exact path="/history" component={History}/>
           <Route exact path="/skills" component={Skills}/>
+          <Route exact path="/projects" component={Projects}/>
         </Switch> 
     </div>
   );
