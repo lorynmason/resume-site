7faf820ef90df4a25d1687a7960c085d8d85a178
Loryn Mason <masonloryn@gmail.com>
2019-05-03 11:06:54 -0700

    Add skills

---
+++ src/Skills/Skills.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Header } from "../Header/Header";
+
+export const Skills = () => {
+  return (
+    <section>
+      <Header/>
+      <section className="skills">
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>SASS/SCSS</li>
+          <li>JAVASCRIPT</li>
+          <li>JQUERY</li>
+          <li>REACT</li>
+          <li>REACT ROUTER V4</li>
+          <li>REACT REDUX</li>
+          <li>NODE.JS</li>
+          <li>EXPRESS</li>
+          <li>PostgreSQL</li>
+          <li>PYTHON 3</li>
+          <li>GIT MERGE/REBASE WORKFLOW</li>
+          <li>AGILE PROCESSES</li>
+          <li>TEST DRIVEN DEVELOPMENT</li>
+          <li>PAIR PROGRAMMING</li>
+          <li>RESPONSIVE DEIGN</li>
+          <li>RESTful APIs</li>
+          <li></li>
+        </ul>
+      </section>
+      <div className='bottom'></div>
+    </section>
+  )
+}
