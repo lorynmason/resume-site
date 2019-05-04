d6f67bfac6f948b0012d5b195947696f7f2d08ac
Loryn Mason <masonloryn@gmail.com>
2019-05-03 20:18:30 -0700

    Create about.js and about route

---
+++ src/About /About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from "../Header/Header";
+
+export const About = () => {
+  return (
+    <section className="about">
+    <Header />
+    </section>
+  )
+}
