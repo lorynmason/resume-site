a3d96b255924a423109484ec1ab922f061220cc9
Loryn Mason <masonloryn@gmail.com>
2019-05-04 10:04:20 -0700

    Fix picture grid

--- src/About /About.js
+++ src/About /About.js
@@ -25,34 +25,41 @@ export const About = () => {
   return (
     <section className="about">
       <Header />
+      <div className="spacer"></div>
       <div className="grid-container">
-        <img src={profile} alt="ME"/>
+        <img src={img2} alt="ME" id="profile"/>
         <article className="bio">
           <p>Motivated developer with significant, real experience
           with current front-end languages and frameworks. Strong analytic and creative ability coupled with excellent team and communication skills.</p>
           <p>Merging degrees in culinary arts and business administration with my experience working in kitchens, I came to software development with strong communication skills in team settings and a desire to keep pushing forward and always be learning. When I’m attacking a problem I patiently study the situation, use my resources, test out theories, and come up with a solution, I’m not afraid to revisit a problem if I come up with a better solution. I’ll obsess over it until I’m satisfied. </p>
         </article>
-          <h1>When I'm not coding...</h1>
+        <h1>When I'm not coding...</h1>
+        <div className="inner-grid">
           <img src={img1} alt=""/>
-          <img src={img2} alt=""/>
+          <img src={img18} alt=""/>
+          {/* <img src={img2} alt=""/> */}
+          <img src={img14} alt=""/>
+
           <img src={img3} alt=""/>
-          <img src={img4} alt=""/>
-          <img src={img5} alt=""/>
-          <img src={img6} alt=""/>
-          <img src={img7} alt=""/>
           <img src={img8} alt=""/>
+          <img src={img13} alt=""/>
+          <img src={img15} alt=""/>
+
+          <img src={img4} alt=""/>
           <img src={img9} alt=""/>
+          <img src={img16} alt=""/>
+
+          <img src={img5} alt=""/>
+          {/* <img src={img6} alt=""/> */}
+          <img src={img12} alt=""/>
           <img src={img10} alt=""/>
           <img src={img11} alt=""/>
-          <img src={img12} alt=""/>
-          <img src={img13} alt=""/>
-          <img src={img14} alt=""/>
-          <img src={img15} alt=""/>
-          <img src={img16} alt=""/>
           <img src={img17} alt=""/>
-          <img src={img18} alt=""/>
-          <img src={img19} alt=""/>
+          <img src={img7} alt=""/>
+          {/* <img src={img19} alt=""/> */}
+        </div>
       </div>
+      <div className="spacer"></div>
     </section>
   )
 }
