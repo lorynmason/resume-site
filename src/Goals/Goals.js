ed90e265dfaed3dc6e3125a72571935b26d0b9d7
Loryn Mason <masonloryn@gmail.com>
2019-05-10 11:47:09 -0700

    Add goals scss and basic structure

--- src/Goals/Goals.js
+++ src/Goals/Goals.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { Header } from "../Header/Header";
 
 export const Goals = () => {
+  const style1 = {
+    "offset": 0,
+    "value": '25%',
+    "background": 'green'
+  }
   return (
     <section>
       <Header/>
+      <div className="spacer"></div>
+      <div className="goals">
+        <div className="goal goal1">job</div>
+        <div className="goal goal2">pull ups</div>
+        <div className="goal goal3">python</div>
+        <div className="goal goal4">coding</div>
+      </div>
     </section>
   )
 }
